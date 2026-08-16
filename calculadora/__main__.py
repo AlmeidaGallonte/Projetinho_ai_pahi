82d7edcfc6e387720500e9be52d097bb0b912832
AlmeidaGallonte <jamersonalmeida4567@gmail.com>
2026-08-16 17:46:04 -0300

    creat main

---
+++ calculadora/__main__.py
@@ -0,0 +1,11 @@
+n1 = int(input('número: '))
+n2 = int(input('número: '))
+ops = str(input('(+),(-),(/),(*): '))
+if ops == '+':
+    print(n1+n2)
+if ops == '-':
+    print(n1-n2)
+if ops == '/':
+    print(n1/n2)
+if ops == '*':
+    print(n1*n2)

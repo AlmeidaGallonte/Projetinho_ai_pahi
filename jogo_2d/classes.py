6e9890ab88090f4e1f8f7f8a71f2255025298a36
AlmeidaGallonte <jamersonalmeida4567@gmail.com>
2026-08-16 17:54:27 -0300

    Upadate classes

---
+++ jogo_2d/classes.py
@@ -0,0 +1,28 @@
+import pygame
+class Jogo():
+    def __init__(self):
+        pygame.init()
+
+
+class Personagem():
+    pass
+
+
+class Item():
+    pass
+
+
+class Player(Personagem):
+    pass
+
+
+class Inimigo(Personagem):
+    pass
+
+
+class Moeda(Item):
+    pass
+
+
+class Parede(Item):
+    pass
